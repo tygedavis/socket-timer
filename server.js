cf987d3235d7f14abecb62141a780be157172c22
tygedavis <tdavis@cmsonline.com>
2020-07-04 16:37:35 -0600

    Server timer function set up

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const io = require('socket.io')();
 
 io.on('connect', (client) => {
-
+  client.on('subscribeToTimer', (interval) => {
+    console.log('Client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+  });
 });
 
 const port = 8000;
