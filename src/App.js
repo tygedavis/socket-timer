cc8538004d66104a07c201ea8ebff3aa8ccfed94
tygedavis <tdavis@cmsonline.com>
2020-07-04 16:51:19 -0600

    Client side timer set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { subscribeToTimer } from './api';
+
 function App() {
+  const [ timestamp, setTimestamp ] = useState('No timestamp yet');
+
+  subscribeToTimer((err, timestamp) => setTimestamp(timestamp));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="App-Intro">
+        This is the timer value: {timestamp}
+      </p>
     </div>
   );
 }
